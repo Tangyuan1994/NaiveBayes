7b48a5bed24bc78a09e91f9b7e63838683fd65a8
Tangyuan1994 <2656995835@qq.com>
2016-12-04 21:28:14 +0100

    try

---
+++ NaiveBayes.py
@@ -0,0 +1,122 @@
+import numpy as np 
+import re
+import pandas as pd
+from nltk.corpus import stopwords
+from sklearn.model_selection import train_test_split
+from collections import Counter
+from nltk.tokenize import word_tokenize
+import collections
+data=pd.read_csv("Tweets.csv")
+data= data.copy()[['airline_sentiment', 'text']]
+# remove the punction and stopwords
+def review_to_words( review ):
+    review_text = review
+    no_punctions = re.sub("[^a-zA-Z]", " ", review_text) 
+    wordslower= no_punctions.lower()
+    words = word_tokenize(wordslower)  
+    stopswd = set(stopwords.words("english"))                  
+    meaningful_wd = [w for w in words if not w in stopswd]
+    return(meaningful_wd)
+
+clean_text = []
+for tweet in data['text']:
+    clean= review_to_words(tweet)
+    clean_text.append(clean)
+
+data['text'] = clean_text
+# 80% of data is data_train for traing,20% of data is data_test for testing
+X_train, X_test, y_train, y_test = train_test_split(data['text'], data['airline_sentiment'], test_size=0.20, random_state=0)
+data_train = pd.DataFrame()
+data_test = pd.DataFrame()
+data_train['text'] = X_train
+data_train['airline_sentiment'] = y_train
+data_train = data_train.reset_index(drop=True)
+data_test['text'] = X_test
+data_test['airline_sentiment'] = y_test
+data_test = data_test.reset_index(drop=True)
+# d is the number of words in positive text,I remember d= 16650
+# e is the number of words in negative text,I remember e= 80297
+# f is is the number of words in neutral text,I remember f= 21642
+a=0
+b=0
+c=0
+e=0
+d=0
+f=0
+dataAll=data_train['text'] 
+copos1=collections.Counter()
+coneg2=collections.Counter()
+coneu3=collections.Counter() 
+dataPos=data_train.copy()[data_train.airline_sentiment == 'positive']
+dataNeg=data_train.copy()[data_train.airline_sentiment == 'negative']
+dataNeu=data_train.copy()[data_train.airline_sentiment == 'neutral']
+pos_words =(dataPos['text'].tolist())
+neg_words =(dataNeg['text'].tolist())
+neu_words =(dataNeu['text'].tolist())
+all_words =(data_train['text'].tolist())
+#calcul the probability of positive text,negative test,and neutral text
+#I remeber P_pos=0.1638,P_neg=0.623975409836,P_neu=0.212175546448
+P_pos = float(len(dataPos))/len(dataAll)
+P_neg = float(len(dataNeg))/len(dataAll)
+P_neu = float(len(dataNeu))/len(dataAll)
+# I use copos,coneg,coneu to divide the training data into 3 emotions
+for i in range(0, len(pos_words)):
+        copos1.update(pos_words[i])
+
+for i in range(0, len(neg_words)):
+        coneg2.update(neg_words[i])
+
+for i in range(0, len(neu_words)): 
+        coneu3.update(neu_words[i])
+
+#calcul the number d,e,f
+for i in range(0, len(pos_words)):
+       for w in pos_words[i]:
+                d=d+1
+
+for i in range(0, len(neg_words)):
+       for w in neg_words[i]:
+                e=e+1
+
+for i in range(0, len(neu_words)):
+       for w in neu_words[i]:
+                f=f+1
+
+#then I predict the result of Testing data
+# the result of predicting including positive,negative and neutral
+class_choice = ['positive', 'negative', 'neutral']
+classification = []      
+test_words =(data_test['text'].tolist())
+# i= the i-th person'sopinion
+for i in range(0, len(test_words)):
+#each word in i-th person's opinion
+       for w in test_words[i]:
+#I think this part maybe have some problems, I use the formulas in the Wikipedia, but there are lots of variables
+#So i'm afraid of this part has some problems,maybe we can talk about this part on monday.
+               a=a+np.log((copos1[w]+1)/float(d))
+               b=b+np.log((coneg2[w]+1)/float(e))
+               c=c+np.log((coneu3[w]+1)/float(f))
+       a=a+np.log(P_pos/(float(1)/len(dataPos)))
+       b=b+np.log(P_neg/(float(1)/len(dataNeg)))
+       c=c+np.log(P_neu/(float(1)/len(dataNeu)))
+#I choose the best results from the training data to predict the testing data
+       probability = (a, b, c)
+       classification.append(class_choice[np.argmax(probability)])
+       a=0
+       b=0
+       c=0
+#I calcul the accuracy
+compare = []
+for i in range(0,len(classification)):
+            if classification[i] == data_test.airline_sentiment.tolist()[i]:
+                value ='correct'
+                compare.append(value)
+            else:
+                value ='incorrect'
+                compare.append(value)
+
+r = Counter(compare)
+accuracy = float(r['correct'])/(r['correct']+r['incorrect'])
+print accuracy
+
+
